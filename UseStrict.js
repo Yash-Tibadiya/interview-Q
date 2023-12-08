26679191ae79e6566ef17ad784cc9708958f88ed
Yash1434 <99134030+Yash-Tibadiya@users.noreply.github.com>
2023-12-08 08:00:48 +0530

    Add files via upload

---
+++ UseStrict.js
@@ -0,0 +1,20 @@
+// variable declaration(you should define the var, let or const)
+
+// "use strict";
+// a = 12; (cannot do like this)
+// var a = 12;
+
+
+// can't delete the variable(you the a without "use strict"; it can't give an error)
+
+// "use strict";
+// var a = 12;
+// delete a;  (cannot delete like this)
+
+
+// can't make same parameters in funcitons
+// use to strict it give error
+"use strict";
+function calculator(a,b,b) {
+
+} 
